40623069c449423fd2844427df0cf82dbf2fd09c
sahasransupati <sahasransu.pati@gmail.com>
2019-06-07 04:03:53 +0000

    Assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,13 @@ import { MatListModule } from '@angular/material/list';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
