92c89257547535e6641c2a925c525382f26d3727
sahasransupati <sahasransu.pati@gmail.com>
2019-06-17 03:18:48 +0000

    Rxjs Part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -18,8 +18,7 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
     
-    this.leaderservice.getLeaders()
-      .then(leaders => this.leaders = leaders);
+    this.leaderservice.getLeaders().subscribe(leaders => this.leaders = leaders);
   }
  
 }
